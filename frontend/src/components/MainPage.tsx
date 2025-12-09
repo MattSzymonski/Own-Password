5dfc77e7bdc3cab150cf1adce2a9572df11419b6
MattSzymonski <mateusz.szymonski@gmail.com>
2025-12-09 17:31:10 +0100

    Fade

--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'motion/react';
 import PasswordFilePicker from './PasswordFilePicker';
 import PasswordFileEditor from './PasswordFileEditor';
 import UnlockDialog from './UnlockDialog';
@@ -11,6 +12,7 @@ export default function MainPage() {
     const [unlockDialogOpen, setUnlockDialogOpen] = useState(false);
     const [dialogCollectionName, setDialogCollectionName] = useState('');
     const [isNewFile, setIsNewFile] = useState(false);
+    const [isTransitioning, setIsTransitioning] = useState(false);
 
     const handleFileSelect = (filename: string) => {
         setDialogCollectionName(filename);
@@ -25,38 +27,60 @@ export default function MainPage() {
     };
 
     const handleUnlocked = (col: PasswoodCollection, password: string, filename: string) => {
-        setCollection(col);
-        setMasterPassword(password);
-        setSelectedFile(filename);
+        setIsTransitioning(true);
+        setTimeout(() => {
+            setCollection(col);
+            setMasterPassword(password);
+            setSelectedFile(filename);
+        }, 300);
+        setTimeout(() => setIsTransitioning(false), 600);
     };
 
     const handleBack = () => {
-        setSelectedFile(null);
-        setCollection(null);
-        setMasterPassword('');
+        setIsTransitioning(true);
+        setTimeout(() => {
+            setSelectedFile(null);
+            setCollection(null);
+            setMasterPassword('');
+        }, 300);
+        setTimeout(() => setIsTransitioning(false), 600);
     };
 
-    if (selectedFile && collection && masterPassword) {
-        return (
-            <PasswordFileEditor
-                filename={selectedFile}
-                initialCollection={collection}
-                initialPassword={masterPassword}
-                onBack={handleBack}
-            />
-        );
-    }
-
     return (
-        <>
-            <PasswordFilePicker onFileSelect={handleFileSelect} onCreateNew={handleCreateNew} />
-            <UnlockDialog
-                open={unlockDialogOpen}
-                onOpenChange={setUnlockDialogOpen}
-                collectionName={dialogCollectionName}
-                isNewFile={isNewFile}
-                onUnlocked={handleUnlocked}
-            />
-        </>
+        <div className="bg-black min-h-screen">
+            <AnimatePresence mode="wait">
+                {selectedFile && collection && masterPassword ? (
+                    <motion.div
+                        key="editor"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: isTransitioning ? 0 : 1 }}
+                        transition={{ duration: 0.2, ease: 'easeInOut' }}
+                    >
+                        <PasswordFileEditor
+                            filename={selectedFile}
+                            initialCollection={collection}
+                            initialPassword={masterPassword}
+                            onBack={handleBack}
+                        />
+                    </motion.div>
+                ) : (
+                    <motion.div
+                        key="picker"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: isTransitioning ? 0 : 1 }}
+                        transition={{ duration: 0.2, ease: 'easeInOut' }}
+                    >
+                        <PasswordFilePicker onFileSelect={handleFileSelect} onCreateNew={handleCreateNew} />
+                        <UnlockDialog
+                            open={unlockDialogOpen}
+                            onOpenChange={setUnlockDialogOpen}
+                            collectionName={dialogCollectionName}
+                            isNewFile={isNewFile}
+                            onUnlocked={handleUnlocked}
+                        />
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </div>
     );
 }
